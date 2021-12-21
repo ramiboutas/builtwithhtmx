51d5eaef44fb0be7e9c4813231cf01d5240c3acb
ramiboutas <ramiboutas@hotmail.com>
2021-12-21 21:27:16 +0100

    header

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = ['www.builtwithhtmx.com', 'builtwithhtmx.com', 'localhost']
+ALLOWED_HOSTS = ['www.builtwithhtmx.com', 'builtwithhtmx.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
